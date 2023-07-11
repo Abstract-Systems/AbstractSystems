52efba2ddaf71cc20105b1d4203bd8c3154de98b
Haider12212 <84367962+Haider12212@users.noreply.github.com>
2023-07-11 14:39:18 +0500

    some more changes

--- abstractsystems/app/layout.js
+++ abstractsystems/app/layout.js
@@ -5,7 +5,11 @@ import Head from 'next/head'
 import Link from 'next/link'
 
 
-
+export const metadata = {
+  title: 'Abstract Systems',
+  description: 'Abstract Systems',
+  image: './images/logo.png',
+}
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
@@ -13,8 +17,7 @@ export default function RootLayout({ children }) {
       <body>
         <Head>
           <title>Abstract Systems</title>
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-
+          
           <Link rel="preconnect" href="https://fonts.gstatic.com"/>
           <Link href="https://fonts.googleapis.com/css2?family=Barlow+Condensed:wght@300;400;500;600;700&display=swap" rel="stylesheet"/>
   
--- abstractsystems/components/Nav.jsx
+++ abstractsystems/components/Nav.jsx
@@ -46,7 +46,7 @@ const Nav = () => {
           } w-full md:flex md:w-auto md:order-1`}
           id="navbar-sticky"
         >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 font-sans tracking-wider ">
+          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 font-sans tracking-widest">
             <li>
               <a
                 href="#"
