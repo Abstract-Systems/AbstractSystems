9b61ed01afc895e787199a697bf31abbb80b5aeb
Haider12212 <84367962+Haider12212@users.noreply.github.com>
2023-07-14 15:54:07 +0500

    Some changes

--- abstractsystems/components/Service.jsx
+++ abstractsystems/components/Service.jsx
@@ -3,20 +3,16 @@ import Image from "next/image";
 
 const Service = (props) => {
   return (
-    <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-md rounded-lg p-6">
-      <div className="flex flex-col md:flex-row md:items-center">
-        <div className="md:mr-4">
-          <img src={props.icon} alt="Icon" className="w-12 h-12" />
+    <div className="bg-white bg-opacity-10 backdrop-filter backdrop-blur-md rounded-lg p-8 text-center">
+      <div className="flex flex-col items-center">
+      <div className="mt-4">
+          <Image src={props.r_image} alt="Image" height={200} width={250} />
         </div>
-        <div className="flex-grow">
-          <div className="text-center md:text-left"> {/* Update to align text center on mobile and left on larger screens */}
-            <h3 className="text-xl font-barlow-condensed">{props.title}</h3>
-            <p className="mt-2">{props.desc}</p>
-          </div>
+        <div>
+          <h3 className="text-xl font-barlow-condensed">{props.title}</h3>
+          <p className="mt-2">{props.desc}</p>
         </div>
-        <div className="mt-4 md:mt-0">
-          <Image src={props.r_image} alt="Image" height={150} width={150} />
-        </div>
+        
       </div>
     </div>
   );
