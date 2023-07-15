f0ea03327121ca4fca073778ace49ef7b3500a43
Haider12212 <84367962+Haider12212@users.noreply.github.com>
2023-07-15 11:12:17 +0500

    fixed blobs for mobile UI and added functionality to scroll btn

--- abstractsystems/components/HeroSection.jsx
+++ abstractsystems/components/HeroSection.jsx
@@ -1,23 +1,49 @@
-import React from 'react';
+'use client'
+import {React, useState, useEffect} from 'react';
 import MainText from './IType';
+import { scroller } from "react-scroll";
 
+const scrolldown = () => {
+  scroller.scrollTo("serviceSection", {
+    duration: 800,
+    delay: 0,
+    smooth: "easeInOutQuart",
+    offset:-50
+  })
+};
 export default function HeroSection() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   return (
     <div className="flex flex-col items-center justify-center h-screen relative">
-      <div className="w-[90%] mx-auto flex flex-col md:flex-row justify-between items-center py-12 pl-20 pr-20">
+      <div className="w-[90%] mx-auto flex flex-col md:flex-row justify-between items-center py-12 pl-4 pr-4 md:pl-20 md:pr-20">
         <div className="text-center">
-          <h1 className="text-6xl font-sarpanch text-white typewriter leading-tight tracking-wide h-16">
+          <h1 className="text-4xl md:text-6xl font-sarpanch text-white typewriter leading-tight tracking-wide mt-10">
             <MainText />
           </h1>
-          <p className="text-2xl font-barlow-condensed text-white mt-8">
-            We believe in harnessing the power of abstraction to bring your digital dreams to life.
+          <p className="text-lg md:text-2xl font-barlow-condensed text-white mt-10 md:mt-10">
+            We believe in harnessing the power of abstraction to bring your digital dreams to life.<br></br>
+            Our team is dedicated to transforming abstract ideas into tangible and innovative digital solutions that exceed your expectations.
           </p>
         </div>
         <div className="w-full md:w-1/2 max-w-[500px] mt-8">
           <img className="w-full h-auto rounded-[50px]" src="./images/computerscreen.png" alt="abstract systems" />
         </div>
       </div>
-      <div className="flex flex-col items-center mt-4 mb-16">
+      <div className="flex flex-col justify-end items-center mt-auto mb-2 md:mt-auto" onClick={scrolldown}>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           className="h-6 w-6 animate-bounce text-pink-500"
@@ -34,9 +60,12 @@ export default function HeroSection() {
         </svg>
         <p className="text-pink-500 mt-1">Scroll down</p>
       </div>
-      <div className="blob top-left"></div>
-<div className="blob bottom-right"></div>
-
+      {!isSmallScreen && (
+        <>
+          <div className="blob top-left"></div>
+          <div className="blob bottom-right"></div>
+        </>
+      )}
     </div>
   );
 }
